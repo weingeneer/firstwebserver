4adec1729ceb312437ee39058ef56dc8356bb27e
weingeneer <denkung@mail.ru>
2022-01-18 22:06:41 +0400

    Generate html table

--- cgi-bin/generatehtml.py
+++ cgi-bin/generatehtml.py
@@ -1,6 +1,8 @@
 from tinyhtml import html, h
 
 html_string = html()(
-    h('table')(h('tr')(h('td')(idx*incr) for idx in range(1, 11)) for incr in range(1, 11))
+    h('body')(
+        h('table')(h('tr')(h('td')(h('a', href_=f'http://{idx*incr}.ru')(idx*incr)) for idx in range(1, 11)) for incr in range(1, 11))
+    )
 )
 print(html_string)
